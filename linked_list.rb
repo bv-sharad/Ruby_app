d2a65a2bc920c69e4a16fb98df37731d76e2a7b0
vermashid <sharad@blogvault.net>
2022-07-18 12:34:30 +0000

    modified after review

--- linked_list.rb
+++ linked_list.rb
@@ -1,12 +1,8 @@
 require_relative "linked_list_node"
-
-# class for the linked list
 class LinkedList
-	#initialization of the linkedlist object
 	def initialize(value = nil)
 		@head = LinkedListNode.new(value, nil)
 	end
-	#method to add elements in the list
 	def addition(value)
 		if @head.value.nil?
 			@head = LinkedListNode.new(value, nil)
@@ -18,7 +14,6 @@ class LinkedList
 			current_node.next_node = LinkedListNode.new(value, nil)
 		end
 	end
-	#method to search an element in the list
 	def find(value)
 		current_node = @head
 		return false if current_node.next_node.nil?
@@ -27,7 +22,6 @@ class LinkedList
 			return current_node if current_node.value == value
 		end
 	end
-	# method to delete an element from the list
 	def deletion(value)
 		current_node = @head
 		return if current_node.nil?
@@ -40,7 +34,6 @@ class LinkedList
 			current_node.next_node = current_node.next_node.next_node
 		end
 	end
-	#method to print the linked list
 	def print_list
 		current_node = @head
 		puts current_node.value
@@ -48,9 +41,9 @@ class LinkedList
 			puts current_node.value
 		end
 	end
-	# modified funtion to write the elements of linked list in output file upon exit
-	def modified_print str
+	def modified_print(str)
 		current_node = @head
+		return if current_node.nil?
 		str = str + "#{current_node.value}"
 		while (current_node = current_node.next_node)
 			str = str + "," if str.present?
@@ -58,7 +51,6 @@ class LinkedList
 		end
 		str
 	end	
-	#method to reverse a linked list
 	def reverse
 		previous = nil
 		current = @head
